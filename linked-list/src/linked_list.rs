365fde8b95840c6fe4e690e13fc199bdf9828b45
4nc3str4l <murielmaths@gmail.com>
2022-06-18 22:13:52 +0200

    Another desperate try to create a linked list OMG I´m super noob

--- linked-list/src/linked_list.rs
+++ linked-list/src/linked_list.rs
@@ -1,17 +1,19 @@
+use std::rc::Rc;
+
 
 pub struct Node<T> {
     value: T,
-    next: Option<Box<Node<T>>>,
+    next: Rc<Option<Node<T>>>,
 }
 
 impl<T> Node<T> {
     pub fn new(val: T) -> Self {
-        Self{value: val, next: None}
+        Self{value: val, next: Rc::new(None)}
     }
 }
 
 pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+    head: Rc<Option<Node<T>>>,
     pub length: usize,
 }
 
@@ -19,21 +21,21 @@ impl<T: std::fmt::Display> LinkedList<T> {
     
     pub fn new() -> Self {
         Self {
-            head: None,
+            head: Rc::new(None),
             length: 0,
         }
     }
 
     pub fn insert(&mut self, data: T) {
         let mut new_node = Node::new(data);
-        match &self.head {
+        match self.head.as_ref() {
             Some(_) => {
-                new_node.next = self.head.take();
-                self.head = Some(Box::new(new_node));
+                new_node.next = self.head.clone();
+                self.head = Rc::new(Some(new_node));
 
             },
             None => {
-                self.head = Some(Box::new(new_node))
+                self.head = Rc::new(Some(new_node))
             },
         };
         self.length += 1;
@@ -44,29 +46,26 @@ impl<T: std::fmt::Display> LinkedList<T> {
         if self.length == 0 || idxp > self.length {
             return None;
         }
-        let mut curr = &self.head;
+
+        let mut curr = Rc::clone(&self.head);
         let mut last = None;
         let mut i = 0;
         
+        // Move the head to the next and just return the last head value
+        if i == 0 {
+            self.head = (curr.clone().as_ref().unwrap().next).clone();
+            self.length -= 1;
+            return Some(curr.unwrap().value);
+        }
+
         while i < idxp {
             last = Some(curr);
-            curr = &curr.unwrap().as_ref().next;
+            curr = curr.unwrap().next;
             i+=1;
         }
 
-        // We have last
-        // We have next
-        // We don't have last or next
-        if idxp == 0 {
-            match &curr.as_ref().unwrap().next {
-                Some(next) => {
-                    self.head = Some(*next);
-                },
-                None => {
-                    self.head = None;
-                },
-            }
-        }
+        // Finish the delete algorithm now that we have
+        // the pointer located at the right spot of the list
 
         let val: T = curr.unwrap().value;
         Some(val)
